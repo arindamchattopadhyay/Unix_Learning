fcc4a34095e8d4f8c604d2513c4b3c147772f03b
arindamchattopadhyay <40838263+arindamchattopadhyay@users.noreply.github.com>
2023-01-03 21:09:18 +0530

    Update cut_examples.sh
    Added the file contents for better understanding

--- cut_examples.sh
+++ cut_examples.sh
@@ -10,3 +10,18 @@ echo "Get all the fields from the 2nd to 4th field onwards in a csv file"
 cut -d "," -f 2-4 country.txt
 echo "Change the delimeter of output"
 cut -d "," -f 1- country.txt --output-delimiter="|"
+<<comm
+cat names
+________________________
+Arindam
+Sachin
+Messi
+Nadal
+Zidane
+cat country.txt
+__________________________
+id,name,population,avg_age,capital
+1,India,142,36,Delhi
+2,China,141,43,Bejing
+3,Japan,29,44,Tokyo
+comm
